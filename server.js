89328aa86d6c90bfbe1594cbb9f4baa000e1cd64
poonamkajal <72147984+poonamkajal@users.noreply.github.com>
2024-06-03 13:38:40 -0400

    fix frontend

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 // app.use(morgan('dev'));
 app.use(cookieParser());
@@ -82,7 +82,7 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 app.use('*', (req, res) => {
